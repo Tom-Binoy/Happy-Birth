5b781008cf7895245932e6d7719c1c3668f90d57
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-02-08 19:35:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
 
-function new_image()
-{
-	fabric.Image.fromURL('BirthdayImage.jpg',function(Img){{
+function new_image(){
+console.log("a")
+    fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
         bio = Img;
         bio.scaleToWidth(800);
-        bio.scaleToHeight(500);
+        bio.scaleToHeight(500);7
         bio.set({
             top : 0 ,
             left : 0
         })
         canvas.add(bio);
-    }
+    })
 	
 }
 
 function start(){
-    
- function playSound(){
-    x.play();
- }	
-})
+    x.play("bdayaudio.mp3");	
+}
